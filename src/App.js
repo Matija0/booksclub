12d5876f2542361cc8a0880d8ecba7d38882b791
Matija0 <90220699+Matija0@users.noreply.github.com>
2023-03-18 21:36:43 +0100

    Commit 2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes,  useParams } from "react-router-dom";
 import About from "./pages/About/About";
 import Home from "./pages/Home/Home";
 import BookList from "./components/BookList/BookList"
-import BookDetails from "./components/BookDetails/BookDetails"
+import BookDetails from "./pages/BookDetails/BookDetails";
 import "./index.css"
+
+
 
 function App() {
   return (
+    
     <div className="App">
     
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/book/:id" element={<BookDetails/>}/>
+            <Route exact path="/" element={<Home/>}/>
+            <Route exact path="/about" element={<About/>}/>
+            <Route path="book">
+              <Route path=":id" element={<BookDetails/>}/>
+            </Route>
+            <Route exact path="/library" element={<Library/>}/>
           </Routes>
         </BrowserRouter>
       
